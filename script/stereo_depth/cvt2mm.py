03683f23ff840cff08fb2d5a70ca0ae2ac22911a
Kenrick Albert <52431800+BT3X@users.noreply.github.com>
2024-04-17 20:22:47 +0800

    Added comments and handled an edge case

--- script/stereo_depth/cvt2mm.py
+++ script/stereo_depth/cvt2mm.py
@@ -6,9 +6,15 @@ import numpy as np
 def main():
     for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
+            # Remove the weird files made by macOS
             if file.startswith('.'):
                 os.remove(os.path.join(root, file))
 
+            # Remove the original 8 bit png files
+            if file == "depth_no_color_map.png":
+                os.remove(os.path.join(root, file))
+
+            # Operate on the Numpy files
             if file == "depth_in_meters.npy":
                 # Load the numpy file
                 depth_file = np.load(os.path.join(root, file))
@@ -19,7 +25,7 @@ def main():
                 # Overwrite the original depth image as a 16 bit png
                 cv.imwrite(os.path.join(root, "depth_no_cm.png"), depth_in_mm)
 
-                # Write to an npy file
+                # Write to a numpy file
                 np.save(os.path.join(root, "depth_in_millimeters.npy"), depth_in_mm)
 
 

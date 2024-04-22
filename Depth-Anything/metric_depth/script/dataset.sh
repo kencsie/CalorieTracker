ac382e0a1725316a3396fcf6e5e46a38a71415d3
root <ken205115331@gmail.com>
2024-04-22 11:09:25 +0000

    new dataset update

--- Depth-Anything/metric_depth/script/dataset.sh
+++ Depth-Anything/metric_depth/script/dataset.sh
@@ -1,5 +1,5 @@
 mkdir -p ../data/food
 pip install gdown
 cd ../data/food
-gdown --fuzzy https://drive.google.com/file/d/1-41bpn1jnRn7NEolDzLG0AmxWiGSeONQ/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/13j1kwPMp_TxC4iGPfnCS30UXZ56mpfez/view?usp=sharing
 tar -xzf food.gz

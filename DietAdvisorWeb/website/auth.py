e5dad85f3799dc8d0a5261d70f2ff45a096f09fe
Trang <minhtrangkl03@gmail.com>
2024-05-13 14:01:49 +0800

    register form update & fix rounding problem

--- DietAdvisorWeb/website/auth.py
+++ DietAdvisorWeb/website/auth.py
@@ -2,22 +2,25 @@ from flask import Blueprint, request, redirect, url_for, render_template, sessio
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+from datetime import datetime
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    def get_user_profile(gender, age, weight, height, physical_activity, weight_option, weight_option_amonut):
+    def get_user_profile(gender, birth, weight, height, physical_activity, weight_option, weight_option_amonut):
         physical_activity_table = {'Sedentary':1.2, 'Lightly active':1.375, 'Moderately active':1.55, 'Very active':1.725, 'Super active':1.9}
         
-        age = float(age)
+        birth_date = datetime(int(birth[2]), int(birth[1]), int(birth[0]))
+        today = datetime.now()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
         weight = float(weight)
         height = float(height)
-        weight_option_amonut = float(weight_option_amonut) * 7700 / 30
+        weight_option_amonut = round(float(weight_option_amonut) * 7700 / 30, 2)
 
         BMR_offset = 5 if gender == 'm' else -161
-        BMR = (10 * weight) + (6.25 * height) - (5 * age) + BMR_offset
-        TDEE = BMR * physical_activity_table[physical_activity]
+        BMR = round((10 * weight) + (6.25 * height) - (5 * age) + BMR_offset, 2)
+        TDEE = round(BMR * physical_activity_table[physical_activity], 2)
 
         return {'gender':gender, 'age':age, 'weight':weight, 'height':height, 'physical_activity_multiplier':physical_activity, 'weight_option':weight_option, 'weight_option_amonut':weight_option_amonut, 'BMR':BMR, 'TDEE':TDEE}
 
@@ -27,7 +30,7 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         gender = request.form.get('gender')
-        age = request.form.get('age')
+        birth = [request.form.get('birth_day'), request.form.get('birth_month'), request.form.get('birth_year')]
         weight = request.form.get('weight')
         height = request.form.get('height')
         physical_activity = request.form.get('physical_activity')
@@ -45,7 +48,7 @@ def register():
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         
         #Get user profile
-        user_profile = get_user_profile(gender, age, weight, height, physical_activity, weight_option, weight_option_amount)
+        user_profile = get_user_profile(gender, birth, weight, height, physical_activity, weight_option, weight_option_amount)
         user_data = {'Username': username, 'Password': hashed_password}
         user_data.update(user_profile)
         
